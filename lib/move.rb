ebc29f1b7f85ab66a814e836c64de34091c1ae5b
stephanie wu <stephanie.wu@wework.com>
2018-02-19 20:51:42 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -18,3 +18,9 @@ end
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
+board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+ 
+update_array_at_with(board, 0, "Red")
